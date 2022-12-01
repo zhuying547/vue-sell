43e2f0fda7a234a8ef8a19fd72a79d605d1148e5
zhuying547 <zhuying547@qq.com>
2022-12-01 21:28:38 +0800

    data mock

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
+const appData = require("./data.json");
+const { seller, goods, ratings } = appData;
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    onBeforeSetupMiddleware: function (devServer) {
+      if (!devServer) {
+        throw new Error("webpack-dev-server is not defined");
+      }
+
+      devServer.app.get("/api/seller", function (req, res) {
+        res.json({ errno: 0, data: seller });
+      });
+      devServer.app.get("/api/goods", function (req, res) {
+        res.json({ errno: 0, data: goods });
+      });
+      devServer.app.get("/api/ratings", function (req, res) {
+        res.json({ errno: 0, data: ratings });
+      });
+    },
+  },
 });
